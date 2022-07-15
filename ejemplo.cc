457200c3a7c962293c76ab3be7cfff13c57a5a99
TheIszer <theiszer@gmail.com>
2022-07-15 16:02:12 -0400

    update

--- ejemplo.cc
+++ ejemplo.cc
@@ -12,8 +12,8 @@ int pi_value(int num_trials)
 	for(uint32_t i=0; i<num_trials; i++){
 		//rand()%(max-min + 1) + min
 		//x = rand(-r,r); 
-		x = rand()%(2*r + 1) - r;
-		y = rand()%(2*r + 1) - r;
+		x = srand();
+		y = srand();
 		if ( (x*x + y*y) <= (r*r)) trials_in_circ++;
 	}
 	pi = 4.0 * ((double) trials_in_circ /(double)num_trials);
